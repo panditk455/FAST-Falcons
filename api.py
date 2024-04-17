ec9743dec411a0b334ab1f8f5f4397520a245dc6
Daniellumbu <128616598+Daniellumbu@users.noreply.github.com>
2024-04-17 13:42:39 -0500

    Create api.py

---
+++ api.py
@@ -0,0 +1,15 @@
+#import psycopg2
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+# To test hompage
+# http://stearns.mathcs.carleton.edu:5137/
+@app.route('/')
+def load_homepage():
+    return render_template("home.html")
+
+
+if __name__ == '__main__':
+    my_port = 5137
+    app.run(host='0.0.0.0', port=my_port, debug=False)
