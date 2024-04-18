76853c65f84291ce50e8456504c25bfcdab40a3e
Daniel Estrada <danielestrada@Daniels-MacBook-Air-5.local>
2024-04-18 17:27:44 -0500

    Merged both apps to have one, changed locations into folders such as app and db. Also added dockerfiles and yml file

--- db/db-setup.sql
+++ db/db-setup.sql
@@ -1,17 +1,15 @@
 -- db-setup.sql
-
-CREATE DATABASE USER_LOGINS
+CREATE DATABASE USER_LOGINS;
 
 USE USER_LOGINS;
 
-CREATE TABLE user(
-    username TEXT,
-    pass_word TEXT
-);
+CREATE TABLE user(username TEXT, pass_word TEXT);
 
-INSERT INTO User (username, pass_word) VALUES ('test_username', 'test_password');
+INSERT INTO
+    user (username, pass_word)
+VALUES
+    ('db', 'randompasswordRN');
 
-CREATE USER 'webapp'@'%' IDENTIFIED BY 'randompasswordRN';
+CREATE USER 'webapp' @'%' IDENTIFIED BY 'randompasswordRN';
 
-GRANT ALL ON USER_LOGINS.* TO 'webapp'@'%';
-
+GRANT ALL ON USER_LOGINS.* TO 'webapp' @'%';
