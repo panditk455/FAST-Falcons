d396e900c3d801600f3283c34550ad209795c22c
Daniel Estrada <danielestrada@Daniels-MacBook-Air-5.local>
2024-04-19 18:28:40 -0500

    When you register, username and password stored in database, when you login, checks if username and password combination is in database, if so, logs in, else try again. Added logged in as in homepage to show they are logged in. If not logged in, when click home icon, takes to log in page

--- app/api.py
+++ app/api.py
@@ -4,6 +4,7 @@ import mysql.connector
 
 
 app = Flask(__name__)
+app.secret_key = 'secretkeyfornow'
 
 # Connect to the mySQL database
 db = mysql.connector.connect(
@@ -26,17 +27,67 @@ def load_selectcharacter():
 
 @app.route('/home')
 def load_homepage():
-    return render_template("home.html")
+    if 'username' in session:
+        username = session['username']
+        return render_template("home.html", username=username)
+    else:
+        return redirect('login')
 
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        # Retrieve the username and password submitted in the registration form
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username already exists in the database
+        cursor.execute(
+            "SELECT * FROM user WHERE username=%s", (username,))
+        existing_user = cursor.fetchone()
+        if existing_user:
+            error_message = "Username already exists! Select new username!"
+            return render_template('register.html', error=error_message)
+
+        # If the username is unique, insert the new user into the database
+        cursor.execute(
+            "INSERT INTO user (username, pass_word) VALUES (%s, %s)", (username, password))
+        db.commit()
+
+        # Store the username in the session to indicate that the user is logged in
+        session['username'] = username
+        return redirect('/home')
     return render_template('register.html')
 
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        # Retrieve the username and password submitted in the login form
+        username = request.form['username']
+        password = request.form['password']
+
+        # Check if the username and password combination exists in the database
+        cursor.execute(
+            "SELECT * FROM user WHERE username=%s AND pass_word=%s", (username, password))
+        user = cursor.fetchone()
+        if user:
+            # If the user exists, store their username in the session to indicate that they are loggin in
+            session['username'] = username
+            return redirect('/')
+        else:
+            # If the user does not exist or the password is incorrect, return an error message
+            error_message = "Invalid username or password! Please try again!"
+            return render_template('login.html',error = error_message)
     return render_template('login.html')
+
+
+@app.route('/logout')
+def logout():
+    # Remove the username from the session to indicate that the user is logged out
+    session.pop('username', None)
+    return redirect('/')
+
 
 '''@app.route('/test')
 def test_db():
