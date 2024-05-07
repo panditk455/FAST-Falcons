c6e6d7d27b84e93ae5b31690e9b7eb219308f615
panditk455 <panditk@carleton.edu>
2024-05-07 00:05:50 -0500

    Added the music elements in the library, needs front end work

--- app/static/js/musicplayer.js
+++ app/static/js/musicplayer.js
@@ -1,42 +1,43 @@
 
 
+
 document.addEventListener("DOMContentLoaded", function () {
-    let audioElement = document.getElementById("mysound");
-    let playPauseIcon = document.getElementById("icon");
-    let songSelector = document.getElementById("songSelector");
-  
 
-    updateIcon();
+  let audioElement = document.getElementById("mysound");
+  let playPauseIcon = document.getElementById("icon");
+  let songSelector = document.getElementById("songSelector");
 
-    playPauseIcon.onclick = function () {
+  updateIcon(); 
+
+
+
+  playPauseIcon.onclick = function () {
       if (audioElement.paused) {
-        audioElement.play();
+          audioElement.play();
       } else {
-        audioElement.pause();
+          audioElement.pause();
       }
       updateIcon();
-    };
-  
-    
-    function updateIcon() {
+  };
+
+
+  function updateIcon() {
       if (audioElement.paused) {
-        playPauseIcon.src = "../static/asset/Play.png"; 
+          playPauseIcon.src = "../static/asset/Play.png";
       } else {
-        playPauseIcon.src = "../static/asset/Pause.png"; 
+          playPauseIcon.src = "../static/asset/Pause.png";
       }
-      }
-  
+    }
 
-    songSelector.addEventListener("change", function () {
+  songSelector.addEventListener("change", function () {
 
-        
       let selectedSong = songSelector.value;
       audioElement.src = "../static/" + selectedSong;
-  
-
+      
       audioElement.pause();
       audioElement.load();
       updateIcon();
-    });
   });
-  
+
+
+});
