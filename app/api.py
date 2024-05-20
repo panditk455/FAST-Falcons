d608647362406ac41211268e06de6bb906d973fe
Daniel Estrada <danielestrada@Daniels-MacBook-Air-5.local>
2024-05-20 13:35:36 -0500

    Added save avatar function in api.py and added new storing for avatar in database

--- app/api.py
+++ app/api.py
@@ -27,7 +27,6 @@ db = mysql.connector.connect(
 cursor = db.cursor()
 
 
-
 @app.route('/requestRoomNum/<name>')
 def requestRoom(name):
     done = False
@@ -44,11 +43,12 @@ def requestRoom(name):
                 done = True
         else:
             done = True
-        
+
     my_dict = {}
     my_dict['num'] = str(room_num)
-    
+
     return json.dumps(my_dict)
+
 
 @app.route('/gameRoom/<num>/<name>')
 def gameRoom(num, name):
@@ -57,22 +57,25 @@ def gameRoom(num, name):
     else:
         return redirect('/login')
     global room_data
-    
+
     # Initialize the room data if it doesn't exist
     if not (num in room_data):
         data = {}
         data['red'] = [" "] * 20  # CHANGED: List of 20 messages instead of 5
-        data['names'] = [name]    # CHANGED: Initialize names list with the provided name
+        # CHANGED: Initialize names list with the provided name
+        data['names'] = [name]
         data['counter'] = 0
         room_data[num] = data
     else:
         data = room_data[num]
-        if name not in data['names']:  # CHANGED: Check if the name is already in the list
+        # CHANGED: Check if the name is already in the list
+        if name not in data['names']:
             data['names'].append(name)
 
     notify_sockets(num)
 
     return render_template("chatroom.html", username=username)
+
 
 @app.route('/getupdate/<num>')
 def returnData(num):
@@ -81,21 +84,23 @@ def returnData(num):
     if num in room_data:
         data_dict = room_data[num]
     else:
-        data_dict = {'red': [" "] * 20, 'names': [], 'counter': 0}  # CHANGED: List of 20 messages instead of 5
-        
+        # CHANGED: List of 20 messages instead of 5
+        data_dict = {'red': [" "] * 20, 'names': [], 'counter': 0}
+
     return json.dumps(data_dict)
 
+
 @app.route('/sendmessage/<num>/<text>/<count>')
-def sendmessage(num, text, count): 
+def sendmessage(num, text, count):
     v = count
-    global currenttext 
+    global currenttext
     global Message
     global room_data
     Message = text
     room_dict = room_data[num]
 
-    if currenttext >= 20:  
-        for i in range(19): 
+    if currenttext >= 20:
+        for i in range(19):
             room_dict['red'][i] = room_dict['red'][i + 1]
         room_dict['red'][19] = Message
     else:
@@ -109,15 +114,22 @@ def sendmessage(num, text, count):
     return json.dumps(room_dict)
 
 # Check if the user has already visited the welcome page
+
+
 def has_visited_welcome(username):
-    cursor.execute("SELECT visited_welcome FROM user WHERE username=%s", (username,))
+    cursor.execute(
+        "SELECT visited_welcome FROM user WHERE username=%s", (username,))
     result = cursor.fetchone()
     return result[0] if result else False
 
 # Update the visited_welcome flag in the database
+
+
 def mark_welcome_visited(username):
-    cursor.execute("UPDATE user SET visited_welcome = 1 WHERE username=%s", (username,))
+    cursor.execute(
+        "UPDATE user SET visited_welcome = 1 WHERE username=%s", (username,))
     db.commit()
+
 
 @app.route('/register', methods=['GET', 'POST'])
 def register():
@@ -159,7 +171,7 @@ def login():
         if user:
             # If the user exists, store their username in the session to indicate that they are logged in
             session['username'] = username
-            
+
             # Check if the user has visited the welcome page
             if not has_visited_welcome(username):
                 mark_welcome_visited(username)
@@ -172,6 +184,7 @@ def login():
             return render_template('login.html', error=error_message)
     return render_template('login.html')
 
+
 @app.route('/welcome')
 def load_welcome():
     if 'username' in session:
@@ -180,6 +193,7 @@ def load_welcome():
     else:
         return redirect('/login')
 
+
 @app.route('/home')
 def load_homepage():
     if 'username' in session:
@@ -187,7 +201,8 @@ def load_homepage():
         return render_template("home.html", username=username)
     else:
         return redirect('/login')
-    
+
+
 @app.route('/sayles')
 def load_sayles():
     if 'username' in session:
@@ -196,6 +211,7 @@ def load_sayles():
     else:
         return redirect('/login')
 
+
 @app.route('/library')
 def load_library():
     if 'username' in session:
@@ -203,7 +219,8 @@ def load_library():
         return render_template("library.html", username=username)
     else:
         return redirect('/login')
-    
+
+
 @app.route('/baldspot')
 def load_baldspot():
     if 'username' in session:
@@ -211,7 +228,8 @@ def load_baldspot():
         return render_template("baldspot.html", username=username)
     else:
         return redirect('/login')
-    
+
+
 @app.route('/profile')
 def load_profile():
     if 'username' in session:
@@ -220,33 +238,36 @@ def load_profile():
     else:
         return redirect('/login')
 
+
 @app.route('/logout')
 def logout():
     # Remove the username from the session to indicate that the user is logged out
     session.pop('username', None)
     return redirect('/login')
 
+
 def notify_sockets(room):
     global web_sockets
 
     dead_sockets = []
-    
-    for num, name  in web_sockets:
+
+    for num, name in web_sockets:
         ws = web_sockets[(num, name)]
         if num == room:
             try:
                 ws.send("Update")
             except:
-                dead_sockets.append( (num, name) )
-                
+                dead_sockets.append((num, name))
+
     for num, name in dead_sockets:
         leave_room(num, name)
+
 
 @app.route('/leaveroom/<num>/<name>')
 def leave_room(num, name):
     global web_sockets
     global room_data
-    
+
     # Remove the WebSocket from the global dictionary
     if (num, name) in web_sockets:
         del web_sockets[(num, name)]
@@ -260,18 +281,36 @@ def leave_room(num, name):
     notify_sockets(num)
     return "Player: " + name + " Left"
 
+
 @sock.route('/openSocket/<num>/<name>')
 def open_socket(ws, num, name):
     global web_sockets
-    
+
     # Add this to the global websocket dictionary
     web_sockets[(num, name)] = ws
-    
+
     # We want to keep the socket open as long as the browser client is active
     while True:
         time.sleep(10)
-    
+
     return ""
+
+
+@app.route('/save_avatar', methods=['POST'])
+def save_avatar():
+    if 'username' in session:
+        username = session['username']
+        avatar_path = request.json.get('avatar_path')
+
+        # Update the avatar path in the database
+        cursor.execute(
+            "UPDATE user SET Avatar_Path = %s WHERE username = %s", (avatar_path, username))
+        db.commit()
+
+        return {'status': 'success'}
+    else:
+        return {'status': 'error', 'message': 'User not logged in'}, 401
+
 
 if __name__ == '__main__':
     my_port = 5555
